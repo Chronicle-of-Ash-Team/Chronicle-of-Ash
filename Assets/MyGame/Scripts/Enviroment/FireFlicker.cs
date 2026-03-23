489f268406b471ccec0a786fed6b55667fe15126
duyd486 <90164590+duyd486@users.noreply.github.com>
2026-03-23 22:52:52 +0700

    add flicker

---
+++ Assets/MyGame/Scripts/Enviroment/FireFlicker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FireFlicker : MonoBehaviour
+{
+    private Light fireLight;
+
+    [SerializeField] private float minPercent = 0.3f;
+
+    private float maxPercent = 1f;
+
+    [SerializeField] private float flickerSpeed = 10f;
+
+    private float baseIntensity;
+
+    void Start()
+    {
+        fireLight = GetComponent<Light>();
+        baseIntensity = fireLight.intensity;
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(Time.time * flickerSpeed, 0f);
+        float percent = Mathf.Lerp(minPercent, maxPercent, noise);
+
+        fireLight.intensity = baseIntensity * percent;
+    }
+}
